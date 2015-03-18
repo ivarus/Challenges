772d92982ca0b72f414dfbfa136d59e1fd449fa6
Ivan Varus <ivan.varus@gmail.com>
2015-03-18 13:41:43 +0200

    final version of codeeval grid walk solution

--- CodeEval/src/Hard/GridWalk.py
+++ CodeEval/src/Hard/GridWalk.py
@@ -6,6 +6,13 @@ Created on Mar 17, 2015
 '''
 
 
+# will return maximum coordinate (x or y) which monkey can reach
+# for given sum of digits.
+def longest_path(sod):
+    path = [9] * (sod // 9) + [sod % 9]
+    return sum(x * 10 ** p for p, x in enumerate(path))
+
+
 def sum_of_digits(num):
     n = abs(num)
     res = 0
@@ -16,41 +23,28 @@ def sum_of_digits(num):
 
 
 def accessible(p):
-    # reduce area to only one quadrant
-    return min(p[0], p[1]) >= 0 and sum(map(sum_of_digits, p)) < 20
-
-
-# will return maximum coordinate (x or y) which monkey can reach
-# for given sum of digits
-def longest_path(sod):
-    n = sod
-    path = []
-    while n > 9:
-        n -= 9
-        path.append(9)
-    path.append(n)
-    ret = 0
-    p = 0
-    for x in path:
-        ret += x * 10 ** p
-        p += 1
-    return ret
+    return sum(map(sum_of_digits, p)) < 20
 
 
 def walk(start):
+    # its very interesting, but tests show, that if we make a set
+    # which we will be checking for visited points will perform faster
+    # or at least as fast as checking in a state matrix by indexes
+    # as points with constant lookup speed
     visited = set()
-    # its very interesting, but tests show that a set with pop()
+    # its very interesting again, but tests show that a set with pop()
     # arbitrary element will perform much faster, than deque
     path = set()
     path.add(start)
-    max_coord = longest_path(20)
     while len(path) > 0:
         p = path.pop()
         visited.add(p)
-        path.update(filter(lambda pp: pp not in visited and accessible(pp),
-            ((p[0], p[1]-1), (p[0], p[1]+1), (p[0]-1, p[1]), (p[0]+1, p[1]))))
+        path.update(filter(lambda pp:  pp not in visited and min(*pp) >= 0 and
+                           accessible(pp), ((p[0], p[1]-1), (p[0], p[1]+1),
+                                            (p[0]-1, p[1]), (p[0]+1, p[1]))))
+    max_len = longest_path(20)
     # 4 times quadrant minus 4 overlapping axes and add center point
-    return len(visited) * 4 - 4 * max_coord + 1
+    return len(visited) * 4 - 4 * max_len + 1
 
 
 if __name__ == '__main__':
